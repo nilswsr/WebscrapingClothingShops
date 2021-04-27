6b2202cfa4d64e69d408e946f19d46dfe8ec9a78
Nils Weißer <kontakt@nilsweisser.de>
2021-04-27 22:09:30 +0200

    Merge Files

--- clothing.py
+++ clothing.py
@@ -1,165 +1,141 @@
-import requests
-from selenium import webdriver
-import re
-import constants
-from bs4 import BeautifulSoup
-from urllib.parse import urlparse
 import json
+from selenium import webdriver
 
 
+def get_data_main(obj):
+    article_id = obj.get_article_id()
+    title = obj.get_title()
+    brand = obj.get_brand()
+    color = obj.get_color()
+    category = obj.get_category()
+    target_gender = obj.get_target_gender()
+    description = obj.get_description()
+    current_price, original_price, discounted = obj.get_price()
+    currency = obj.get_currency()
+    images = obj.get_images()
+    review_count, avrg_rating = obj.get_reviews()
+    return {"articleId": article_id,
+            "title": title,
+            "brand": brand,
+            "color": color,
+            "category": category,
+            "targetGender": target_gender,
+            "description": description,
+            "price": {
+                "currentPrice": current_price,
+                "originalPrice": original_price,
+                "isDiscounted": discounted
+            },
+            "currency": currency,
+            "images": images,
+            "reviews": {
+                "reviewCount": review_count,
+                "averageRating": avrg_rating
+            }
+            }
 
-class ZalandoScraping2:
+
+class ZalandoScraping:
     def __init__(self, url):
         self.url = url
-        self.text = self.__fetch_page(self.url)
-        self.soup = BeautifulSoup(self.text, 'html.parser')
+        self.product_data = self.__fetch_page()
 
     def __fetch_page(self):
-        session = requests.Session()
-        headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
-                          "Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"}
-        response = session.get(self.url, headers=headers)
-        return response.text
+        driver = webdriver.Firefox()
+        driver.get(self.url)
+        product_data = driver.execute_script("return document.getElementById('z-vegas-pdp-props').textContent")
+        driver.close()
+        return json.loads(product_data[product_data.index("{"):product_data.rindex("}")+1])["model"]["articleInfo"]
 
-    def __get_title(self):
-        return self.soup.find("h1", {"class": constants.ZA_C_TITLE}).text
+    def get_article_id(self):
+        return self.product_data["id"]
 
-    def __get_brand(self):
-        return self.soup.find("h3", {"class": constants.ZA_C_BRAND}).text
+    def get_title(self):
+        return self.product_data["name"]
 
-    def __get_currency(self):
-        tld = urlparse(self.url).netloc.replace("www.zalando.", "").split(":")[0]
-        return constants.CURRENCIES[tld.upper()]
+    def get_brand(self):
+        return self.product_data["brand"]["name"]
 
-    def __get_price(self):
-        for elem in self.soup.find_all("span", {"class": constants.ZA_C_PRICE}):
-            if elem.text != "":
-                value = float(re.findall("([0-9]+(?:.|,)[0-9]+)", elem.text)[0].replace(",", "."))
-                currency = self.__get_currency()
-                return [value, currency]
+    def get_category(self):
+        return self.product_data["product_group"]
 
-    def __get_color(self):
-        div = self.soup.find("div", {"class": constants.ZA_C_COLORLST})
-        return div.find("span", {"class": constants.ZA_C_COLOR}).text
+    def get_target_gender(self):
+        return self.product_data["targetGroups"]["gender"][0]
 
-    def __get_images(self):
-        ul = self.soup.find("ul", {"class": constants.ZA_C_IMGLST})
-        return [x["src"].split("=")[0] + "=1080" for x in ul.find_all("img", {"class": constants.ZA_C_IMG})]
+    def get_color(self):
+        return self.product_data["color"]
 
-    def __get_condition(self):
-        for p in constants.PREOWNED_KEYWORDS:
-            if p in self.__get_title():
-                return p
-        return "new"
+    def get_description(self):
+        return None
 
-    def __get_reviews(self):
-        review_row = self.soup.find_all("div", {"id": constants.ZA_ID_REVIEW})
-        if len(review_row):
-            count = int(re.findall(r"\(\d+\)", review_row[0].find("h5").text)[0][1:-1])
-            if count:
-                rating = float(review_row[0].find("span", {"class": constants.ZA_C_RATING}).text)
-                return [count, rating]
-        return [0, "NaN"]
+    def get_price(self):
+        current_price = self.product_data["units"][0]["displayPrice"]["price"]["value"]
+        original_price = self.product_data["units"][0]["displayPrice"]["originalPrice"]["value"]
+        discounted = self.product_data["units"][0]["displayPrice"]["isDiscounted"]
+        return current_price, original_price, discounted
 
-    def __get_moreinfo(self, position):
-        infos = {}
-        moreinfo_section = self.soup.find_all("div", {"class": constants.ZA_C_INFOMAIN})[position]
-        subsection = moreinfo_section.find("div", {"class": constants.ZA_C_INFOSUB})
-        rows = subsection.find_all("span", {"class": constants.ZA_C_INFOROW})
-        if len(rows) % 2 != 0:
-            infos["Info"] = rows[0].text
-            rows.pop(0)
-        for i in range(0, len(rows), 2):
-            attribute = rows[i].text.replace(":", "")
-            value = rows[i + 1].text
-            infos[attribute] = value
-        return infos
+    def get_currency(self):
+        return self.product_data["units"][0]["displayPrice"]["price"]["currency"]
 
-    def __get_material(self):
-        return self.__get_moreinfo(0)
+    def get_images(self):
+        image_lst = self.product_data["media"]["images"]
+        return [x["sources"]["zoom"] for x in image_lst]
 
-    def __get_details(self):
-        return self.__get_moreinfo(1)
-
-    def __get_sizeinfo(self):
-        return self.__get_moreinfo(2)
+    def get_reviews(self):
+        return int(self.product_data["reviewsCount"]), float(self.product_data["averageStarRating"])
 
     def get_data(self):
-        title = self.__get_title()
-        brand = self.__get_brand()
-        price, currency = self.__get_price()
-        color = self.__get_color()
-        condition = self.__get_condition()
-        review_count, rating = self.__get_reviews()
-        images = self.__get_images()
-        details = {}
-        for d in [self.__get_material(), self.__get_details(), self.__get_sizeinfo()]:
-            details.update(d)
-        return {"title": title, "brand": brand, "price": price, "currency": currency, "color": color,
-                "condition": condition, "review_count": review_count, "rating": rating,
-                "details": details, "images": images}
+        return get_data_main(self)
 
 
 class HMScraping:
     def __init__(self, url):
         self.url = url
-        self.product_details = self.__fetch_page()
-        self.soup = BeautifulSoup(self.text, 'html.parser')
+        self.product_data, self.product_details = self.__fetch_page()
 
     def __fetch_page(self):
         driver = webdriver.Firefox()
         driver.get(self.url)
-        #html = driver.execute_script("return document.documentElement.outerHTML")
-        product_data = driver.execute_script("return productArticleDetails[hm.product.getArticleId()]")
-        #print(json.dumps(test, indent=2, ensure_ascii=False))
+        product_data = driver.execute_script("return document.getElementById('product-schema').textContent")
+        product_details = driver.execute_script("return productArticleDetails[hm.product.getArticleId()]")
         driver.close()
-        return product_data
+        return json.loads(product_data), product_details
 
-    def __get_title(self):
-        return self.soup.find("h1", {"class": constants.HM_C_TITLE}).text
+    def get_article_id(self):
+        return self.product_data["sku"]
 
-    def __get_currency(self):
-        country = self.soup.find("html")["lang"].split("-")[-1]
-        return constants.CURRENCIES[country]
+    def get_title(self):
+        return self.product_data["name"]
 
-    def __get_price(self):
-        price_div = self.soup.find("div", {"class": "ProductPrice-module--productItemPrice__2rpyB"})
-        print(self.soup.find("section", {"class": "name-price"}))
-        value = re.findall("[0-9]+(?:.|,)[0-9]+", str(price_div.find("span").text))
-        return value, self.__get_currency()
+    def get_brand(self):
+        return self.product_data["brand"]["name"]
 
-    def __get_color(self):
-        return self.soup.find("h3", {"class": constants.HM_C_COLOR}).text
+    def get_category(self):
+        return self.product_data["category"]["name"]
 
-    def __get_images(self):
-        return [x["src"] for x in self.soup.find_all("img", {"class": constants.HM_C_IMG})]
+    def get_target_gender(self):
+        return None
 
-    def __get_reviews(self):
-        """
-        print(self.soup.find("h2", {"id": constants.HM_ID_REVIEW}).text)
-        review_count = re.findall(r"\(\d+\)", str(self.soup.find("h2", {"id": constants.HM_ID_REVIEW}).text))[0][1:-1]
-        if not review_count:
-            return [0, "NaN"]
-        rating = self.soup.find("div", {"class": constants.HM_C_RATING})
-        return review_count, rating
-        """
-        print(self.soup.find("div", {"class": "sticky-footer"}))
-        print(self.soup.find_all("h2", {"id": "js-review-heading"}))
-        review_span = self.soup.find_all("h2", {"id": "js-review-heading"})
-        review_count = re.findall(r"\(\d+\)", str(self.soup.find("span", {"class": "reviews-number"}).text))[0][1:-1]
-        if not review_count:
-            return [0, "NaN"]
-        rating = self.soup.find("div", {"class": constants.HM_C_RATING})
-        return review_count, rating
+    def get_color(self):
+        return self.product_data["color"]
+
+    def get_description(self):
+        return self.product_data["description"]
+
+    def get_price(self):
+        current_price = self.product_data["offers"][0]["price"]
+        original_price = current_price
+        discounted = False
+        return current_price, original_price, discounted
+
+    def get_currency(self):
+        return self.product_data["offers"][0]["priceCurrency"]
+
+    def get_images(self):
+        return [x["image"] for x in self.product_details["images"]]
+
+    def get_reviews(self):
+        return None, None
 
     def get_data(self):
-        title = self.__get_title()
-        brand = "H&M"
-        price, currency = self.__get_price()
-        color = self.__get_color()
-        condition = "new"
-        review_count, rating = self.__get_reviews()
-        images = self.__get_images()
-        return {"title": title, "brand": brand, "price": price, "currency": currency, "color": color,
-                "condition": condition, "review_count": review_count, "rating": rating, "images": images}
+        return get_data_main(self)
--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import json
-from Zalando import ZalandoScraping
-from HM import HMScraping
+from clothing import ZalandoScraping, HMScraping
 
-"""
+
 # zalando_item = ZalandoScraping("https://www.zalando.de/bronx-jaxstar-schnuerstiefel-cappuccino-br111a07a-b11.html")
 zalando_item = ZalandoScraping("https://www.zalando.de/soliver-im-cleanen-t-shirt-basic-blush-so221d225-j12.html")
 zalando_data = zalando_item.get_data()
 zalando_json = json.dumps(zalando_data, indent=2, ensure_ascii=False)
 print(zalando_json)
-"""
+
 
 hm_item = HMScraping("https://www2.hm.com/de_de/productpage.0950532001.html")
 #hm_item = HMScraping("https://www2.hm.com/de_de/productpage.0685813042.html")
